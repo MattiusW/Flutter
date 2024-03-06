6d8d422b0ad282151ff3aef3c29c9a25c6315b91
Mateusz Walczyk <mattiusw@gmail.com>
2024-03-06 18:30:33 +0100

    Add app bar

--- first_app/lib/pages/home.dart
+++ first_app/lib/pages/home.dart
@@ -5,6 +5,16 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First app',
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold)),
+        backgroundColor: Colors.deepPurple,
+        centerTitle: true,
+      ),
+    );
   }
 }

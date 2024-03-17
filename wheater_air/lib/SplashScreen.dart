5cdae862bdc80cf6816724ac584861139e7ce814
Mateusz Walczyk <mattiusw@gmail.com>
2024-03-17 09:18:43 +0100

    Set widget

--- wheater_air/lib/SplashScreen.dart
+++ wheater_air/lib/SplashScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key, required this.title});
@@ -13,12 +14,30 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(),
+          Align(
+              alignment: FractionalOffset.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.cloud, color: Colors.black),
+                  Padding(padding: EdgeInsets.only(top: 15.0)),
+                  Text(widget.title),
+                  Padding(padding: EdgeInsets.only(top: 5.0)),
+                  Text('Aplikacja do monitorowania \n czystości powietrza'),
+                ],
+              )),
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 35,
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Text("Przewiewam dane...")))
+        ],
       ),
     );
   }

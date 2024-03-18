eadcb8655f57f481da56a4435ef08f729ee0f12a
Mateusz Walczyk <mattiusw@gmail.com>
2024-03-18 12:16:45 +0100

    Swtich with widget

---
+++ wheater_air/lib/AirScreen.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:wheater_air/MyHomePage.dart';
+import 'package:wheater_air/PermissionScreen.dart';
+import 'package:wheater_air/main.dart';
+
+class AirScreen extends StatefulWidget {
+  @override
+  State<AirScreen> createState() => _AirScreenState();
+}
+
+class _AirScreenState extends State<AirScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+              decoration: BoxDecoration(
+                  color: new Color(0xffffffff),
+                  gradient: LinearGradient(
+                      begin: Alignment.centerRight,
+                      end: Alignment.centerLeft,
+                      colors: [new Color(0xff6671e5), new Color(0xff4852d9)]))),
+          Align(
+              alignment: FractionalOffset.center,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image(image: AssetImage('icons/cloud-sun.png')),
+                  Padding(padding: EdgeInsets.only(top: 15.0)),
+                  Text(Strings.appTitle,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                          textStyle: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 42.0,
+                        color: Colors.white,
+                      ))),
+                  Padding(padding: EdgeInsets.only(top: 5.0)),
+                  Text('Powietrze',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                          textStyle: TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      )))
+                ],
+              )),
+          Positioned(
+              left: 0,
+              right: 0,
+              bottom: 35,
+              child: Container(
+                  alignment: Alignment.center,
+                  child: Text("Przewiewam dane...",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.lato(
+                          fontWeight: FontWeight.w300,
+                          textStyle: TextStyle(
+                            fontSize: 18.0,
+                            color: Colors.white,
+                          )))))
+        ],
+      ),
+    );
+  }
+
+  bool havePermission() {
+    return true;
+  }
+}
--- wheater_air/lib/MyHomePage.dart
+++ wheater_air/lib/MyHomePage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:wheater_air/AirScreen.dart';
+import 'package:wheater_air/WeatherScreen.dart';
 
 class MyHomePage extends StatefulWidget {
   @override
@@ -6,18 +8,28 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var _currentIndex = 0;
+  var screens = [
+    AirScreen(),
+    WheatherScreen(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home"),
+      body: IndexedStack(
+        index: _currentIndex,
+        children: screens,
       ),
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.masks_outlined), label: "Powietrze"),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.wb_cloudy_outlined), label: "Pogoda")
-      ]),
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) => setState(() => _currentIndex = index),
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.masks_outlined), label: "Powietrze"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.wb_cloudy_outlined), label: "Pogoda")
+          ]),
     );
   }
 }

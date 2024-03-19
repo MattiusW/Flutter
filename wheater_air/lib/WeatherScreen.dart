cf4c36865275a073e6b090945d9df8db24c6e4e2
Mateusz Walczyk <mattiusw@gmail.com>
2024-03-19 11:33:10 +0100

    Style widget weather

--- wheater_air/lib/WeatherScreen.dart
+++ wheater_air/lib/WeatherScreen.dart
@@ -21,9 +21,13 @@ class _WheatherScreenState extends State<WheatherScreen> {
               decoration: BoxDecoration(
                   color: new Color(0xffffffff),
                   gradient: LinearGradient(
-                      begin: Alignment.centerRight,
-                      end: Alignment.centerLeft,
-                      colors: [new Color(0xff6e6cd8), new Color(0xff77e1ee)]))),
+                      begin: Alignment.bottomLeft,
+                      end: Alignment.topRight,
+                      colors: [
+                        new Color(0xff6e6cd8),
+                        new Color(0xff40a0ef),
+                        new Color(0xff77e1ee)
+                      ]))),
           Align(
               alignment: FractionalOffset.center,
               child: Column(
@@ -32,10 +36,38 @@ class _WheatherScreenState extends State<WheatherScreen> {
                   Padding(padding: EdgeInsets.only(top: 45.0)),
                   Image(image: AssetImage('icons/weather-sunny.png')),
                   Padding(padding: EdgeInsets.only(top: 41.0)),
-                  Text("Poniedziałek 31.03, 18:00 słonecznie"),
+                  Text(
+                    "Poniedziałek 31.03, 18:00 słonecznie",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                            fontSize: 14.0,
+                            height: 1.2,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400)),
+                  ),
                   Padding(padding: EdgeInsets.only(top: 12.0)),
-                  Text('14°C'),
-                  Text('Odczuwalna 13°C'),
+                  Text(
+                    '14°C',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                            fontSize: 64.0,
+                            height: 1.2,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400)),
+                  ),
+                  Text(
+                    'Odczuwalna 13°C',
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                            fontSize: 14.0,
+                            height: 1.2,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400)),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 25.0)),
                   IntrinsicHeight(
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -45,24 +77,61 @@ class _WheatherScreenState extends State<WheatherScreen> {
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              Text('Ciśnienie'),
+                              Text(
+                                'Ciśnienie',
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontSize: 14.0,
+                                        height: 1.2,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w300)),
+                              ),
                               Padding(padding: EdgeInsets.only(top: 2.0)),
-                              Text('102- hPa')
+                              Text(
+                                '1020 hPa',
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontSize: 26.0,
+                                        height: 1.2,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700)),
+                              )
                             ],
                           ),
                         ),
                         VerticalDivider(
                           width: 48,
                           thickness: 2,
+                          color: Colors.white,
                         ),
                         Container(
                           width: 130,
                           child: Column(
                             mainAxisAlignment: MainAxisAlignment.end,
                             children: [
-                              Text('Wiatr'),
+                              Text(
+                                'Wiatr',
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontSize: 14.0,
+                                        height: 1.2,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w300)),
+                              ),
                               Padding(padding: EdgeInsets.only(top: 2.0)),
-                              Text('16 km/h')
+                              Text(
+                                '16 km/h',
+                                textAlign: TextAlign.center,
+                                style: GoogleFonts.lato(
+                                    textStyle: TextStyle(
+                                        fontSize: 26.0,
+                                        height: 1.2,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.w700)),
+                              )
                             ],
                           ),
                         ),
@@ -70,7 +139,16 @@ class _WheatherScreenState extends State<WheatherScreen> {
                     ),
                   ),
                   Padding(padding: EdgeInsets.only(top: 24.0)),
-                  Text("Opady: 0,1 mm/12h"),
+                  Text(
+                    "Opady: 0,1 mm/12h",
+                    textAlign: TextAlign.center,
+                    style: GoogleFonts.lato(
+                        textStyle: TextStyle(
+                            fontSize: 14.0,
+                            height: 1.2,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w400)),
+                  ),
                   Padding(padding: EdgeInsets.only(top: 68.0)),
                 ],
               )),
